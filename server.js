add32823d20a2b54517a6022959fe86ec0678349
Alex <alexander.tastad@hotmail.com>
2024-12-22 20:47:36 +0100

    Fix: sharing the link

--- server.js
+++ server.js
@@ -65,3 +65,11 @@ app.post('/get-video', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
